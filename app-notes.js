769658c8e27e5e2f1023353c79f25a4cad1ba28d
ninozivan <n.maxnova@gmail.com>
2018-05-31 13:28:41 +0200

    First setup of project
    Created vue webpack template for first setup

---
+++ app-notes.js
@@ -0,0 +1,8 @@
+/*
+
+    AIR TRAFFIC APP - WHAT TO DO ORDER:
+    1. Learn how "ADS-B Exchange API" works
+    2. Create simple application that will get user geolocation
+    3. Use geolocation and send/receive air traffic information from API
+    4. After main functionalities are ready, create UI wireframes and html/css
+*/
